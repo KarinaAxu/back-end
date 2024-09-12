23a855271c9df5643f0117272a251b01dc7e1e22
KarinaAxu <156989064+KarinaAxu@users.noreply.github.com>
2024-09-12 22:42:13 +0500

    first commit2

---
+++ store/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Product)
+admin.site.register(models.Restaurant)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
+admin.site.register(models.Courier)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
+admin.site.register(models.Address)
+admin.site.register(models.Payment)
